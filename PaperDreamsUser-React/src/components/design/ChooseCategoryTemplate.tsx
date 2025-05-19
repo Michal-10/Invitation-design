a11b2f87ffd3c8f949b4afbb092b995ea7ddb7bb
Michal-10 <m0548564934@gmail.com>
2025-05-20 00:20:15 +0300

    update

--- PaperDreamsUser-React/src/components/design/ChooseCategoryTemplate.tsx
+++ PaperDreamsUser-React/src/components/design/ChooseCategoryTemplate.tsx
@@ -25,6 +25,8 @@ export default function ChooseCategoryPage() {
         setLoading(true)
         console.log("before getCategories");
         console.log(`${import.meta.env.VITE_API_URL}/Category`);
+        console.log("meta.env");
+        console.log(import.meta.env);
         
         console.log("------------------");
         
