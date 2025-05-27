b7fc1e51776aa34149eb8de8cb7fdc9fe182b22e
Michal-10 <m0548564934@gmail.com>
2025-05-27 11:23:09 +0300

    update

--- PaperDreams-AdminClient/src/environments/environment.prod.ts
+++ PaperDreams-AdminClient/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://invitationline-server.onrender.com/api'
-    // apiUrl: 'https://invitationline-server-ht5o.onrender.com'
+    // apiUrl: 'https://invitationline-server.onrender.com/api'
+    apiUrl: 'https://invitationline-server-ht5o.onrender.com'
   };
   
